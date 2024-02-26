74d9dd0d6c879fac674491072a7221e5f9bc8268
JuniorDevelop01 <bahar_n@outlook.co.id>
2024-02-26 19:45:15 +0700

    edit file main.py

--- lambda/main.py
+++ lambda/main.py
@@ -1,6 +1,9 @@
 def handler(event, context):
+    # Proses event sesuai kebutuhan Anda
     print(event)
+    
+    # Kirim respons dengan status code 200 dan pesan "Hello World!"
     return {
-        "StatusCode": 200,
+        "statusCode": 200,
         "body": "Hello World!"
     }
