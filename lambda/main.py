a7b39465c5b90c4e409a7949f60dfa944cda39e4
JuniorDevelop01 <bahar_n@outlook.co.id>
2024-02-26 21:30:49 +0700

    add script in file aws-cicd-tutorial-stack.ts and main.py

--- lambda/main.py
+++ lambda/main.py
@@ -1,10 +1,31 @@
 import os
+import boto3
 
 def handler(event, context):
-    version = os.environ.get("VERSION", "0.0")
+
+    # Raw event data
+    path = event["rawPath"]
+    if path != "/":
+        return {"statusCode": 404, "body": "Not found"}
+
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(os.environ.get("TABLE_NAME"))
+
+    # Read the "VISIT COUNT" key (or create it if it doesn't exist)
+    response = table.get_item(Key={"key": "visit_count"})
+    if "Item" in response:
+        visit_count = response["Item"]["value"]
+    else:
+        visit_count = 0
+
+    # Increment the visit count and write it back to the table.
+    new_visit_count = visit_count + 1
+    table.put_item(Item={"key": "visit_count", "value": new_visit_count})
+
+    version = os.environ.get("VERSION", "1.0.0")  # Fixed here
     response_body = {
         "message": "Hello World",
-        "version": "1.0.0"
+        "version": version
     }
     
     # Kirim respons dengan status code 200 dan menggunakan response_body
--- lib/aws-cicd-tutorial-stack.ts
+++ lib/aws-cicd-tutorial-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { LambdaDataSource } from "aws-cdk-lib/aws-appsync";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as dotenv from "dotenv";
 
 export class AwsCicdTutorialStack extends cdk.Stack {
@@ -11,14 +12,26 @@ export class AwsCicdTutorialStack extends cdk.Stack {
     // Load the environment .env file.
     dotenv.config();
 
+    // Create a table to store some date
+    const table = new dynamodb.Table(this, "VisitorTimeTable", {
+      partitionKey: {
+        name: "key",
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    })
+
     const lambdaFunction = new lambda.Function(this, "LambdaFunction", {
       runtime: lambda.Runtime.PYTHON_3_9,
       code: lambda.Code.fromAsset("lambda"),
       handler: "main.handler",
       environment: {
         VERSION: process.env.VERSION || "0.0",
+        TABLE_NAME: table.tableName,
       },
     });
+
+    table.grantReadWriteData(lambdaFunction);
 
     const functionUrl = lambdaFunction.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
